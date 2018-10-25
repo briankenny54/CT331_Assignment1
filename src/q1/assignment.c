12e2512f25dc8a3d61d21b2060fd9218fa013e4a
BRIAN  KENNY <16397953@it.nuigalway.ie>
2018-10-25 13:35:10 +0100

    finished Q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+ 	int a;
+	int *b;
+	long c;
+	double *d;
+	char **e;
+	
+  printf("%d %d %d %d %d ", sizeof(a), sizeof(b), sizeof(c), sizeof(d), sizeof(e));
   return 0;
 }
